5ee6bf23f4e24fd3618c5d6b92dbbdc8a4143852
Shawn Throop <shawnthroop@gmail.com>
2014-09-10 09:18:09 +0200

    Site updated at 2014-09-10 07:18:09 UTC

--- javascripts/octopress.js
+++ javascripts/octopress.js
@@ -6,7 +6,7 @@ function getNav() {
     mobileNav.find('select').append('<option value="' + this.href + '">&raquo; ' + $(this).text() + '</option>');
   }
   mainNav.find('a').each(addOption);
-  $('ul.subscription a').each(addOption);
+  //$('ul.subscription a').each(addOption);
   mobileNav.find('select').bind('change', function(event) {
     if (event.target.value) { window.location.href = event.target.value; }
   });
@@ -111,7 +111,7 @@ $('document').ready(function() {
   flashVideoFallback();
   addCodeLineNumbers();
   getNav();
-  addSidebarToggler();
+  //addSidebarToggler();
 });
 
 // iOS scaling bug fix
